233726f9b916a8dcf3a38d8bf5adcd8397aa70c0
gundam1337 <omarderkaoui1337@gmail.com>
2023-09-25 20:51:06 +0100

    frist try to include Formik lib to the project

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Formik, Form, useField, useFormikContext } from "formik";
 import * as Yup from "yup";
 import "../../styles/register.css";
@@ -33,10 +33,9 @@ const MyCheckbox = ({ children, ...props }) => {
   );
 };
 
-
 const Registration = (props) => {
   return (
-    <div>
+    <>
       <div class="form-container">
         <button class="close" onClick={props.onClick}>
           &times;
@@ -46,170 +45,228 @@ const Registration = (props) => {
           <h3>Create an account</h3>
           <p>We need informations to help you to found your Matcha</p>
 
-
-          <form action="" method="POST">
-            <h3>Tell us more about yourself</h3>
-            <div class="main-informations">
-              <input name="name " type="text" placeholder="Name" />
-              <input type="email" placeholder="Email" name="mail" />
-              <input type="password" placeholder="Password" name="password" />
-              <h4>Your birthday :</h4>
-              <input type="text" placeholder="MM/DD/YYYY" />
-            </div>
-
-            <div class="gender radio">
-              <h4>Your gender :</h4>
-              <div>
-                <label>
-                  <input type="radio" name="user-gender" value="man" checked />
-                  <span>Man</span>
-                </label>
+          <Formik
+            initialValues={{
+              name: "",
+              email: "",
+              password: "",
+              birthday : "",
+              gender: "",
+              search: "",
+              hobbies : "",
+            }}
+            validationSchema={Yup.object({
+              firstName: Yup.string()
+                .max(15, "Must be 15 characters or less")
+                .required("Required"),
+              lastName: Yup.string()
+                .max(20, "Must be 20 characters or less")
+                .required("Required"),
+              email: Yup.string()
+                .email("Invalid email addresss`")
+                .required("Required"),
+              acceptedTerms: Yup.boolean()
+                .required("Required")
+                .oneOf([true], "You must accept the terms and conditions."),
+              jobType: Yup.string()
+                // specify the set of valid values for job type
+                // @see http://bit.ly/yup-mixed-oneOf
+                .oneOf(
+                  ["designer", "development", "product", "other"],
+                  "Invalid Job Type"
+                )
+                .required("Required"),
+            })}
+            onSubmit={async (values, { setSubmitting }) => {
+              await new Promise((r) => setTimeout(r, 500));
+              setSubmitting(false);
+            }}
+          >
+            <form action="" method="POST">
+              <h3>Tell us more about yourself</h3>
+              <div class="main-informations">
+                <input name="name " type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" name="mail" />
+                <input type="password" placeholder="Password" name="password" />
+                <h4>Your birthday :</h4>
+                <input type="text" placeholder="MM/DD/YYYY" />
               </div>
 
-              <div>
-                <label>
-                  <input type="radio" name="user-gender" value="woman" />
-                  <span>Woman</span>
-                </label>
-              </div>
-            </div>
+              <div class="gender radio">
+                <h4>Your gender :</h4>
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      name="user-gender"
+                      value="man"
+                      checked
+                    />
+                    <span>Man</span>
+                  </label>
+                </div>
 
-            <h4>You Search</h4>
-            <div class="gender radio">
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    name="search-gender"
-                    value="man"
-                    checked
-                  />
-                  <span>Man</span>
-                </label>
-              </div>
-
-              <div>
-                <label>
-                  <input type="radio" name="search-gender" value="woman" />
-                  <span>Woman</span>
-                </label>
-              </div>
-            </div>
-
-            <h4>Your Hobbies :</h4>
-            <div class="hobbies radio">
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="cooking" />
-                  <span>Cooking</span>
-                </label>
+                <div>
+                  <label>
+                    <input type="radio" name="user-gender" value="woman" />
+                    <span>Woman</span>
+                  </label>
+                </div>
               </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="sport" />
-                  <span>Sport</span>
-                </label>
+              <h4>You Search</h4>
+              <div class="gender radio">
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      name="search-gender"
+                      value="man"
+                      checked
+                    />
+                    <span>Man</span>
+                  </label>
+                </div>
+
+                <div>
+                  <label>
+                    <input type="radio" name="search-gender" value="woman" />
+                    <span>Woman</span>
+                  </label>
+                </div>
               </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="reading" />
-                  <span>Reading</span>
-                </label>
-              </div>
+              <h4>Your Hobbies :</h4>
+              <div class="hobbies radio">
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="cooking"
+                    />
+                    <span>Cooking</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="music" />
-                  <span>Music</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input type="checkbox" name="user-hobbies" value="sport" />
+                    <span>Sport</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="dance" />
-                  <span>Dance</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="reading"
+                    />
+                    <span>Reading</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="user-hobbies"
-                    value="astronomy"
-                  />
-                  <span>Astronomy</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input type="checkbox" name="user-hobbies" value="music" />
+                    <span>Music</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="user-hobibes"
-                    value="gardening"
-                  />
-                  <span>Gardening</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input type="checkbox" name="user-hobbies" value="dance" />
+                    <span>Dance</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="user-hobbies"
-                    value="photography"
-                  />
-                  <span>Photography</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="astronomy"
+                    />
+                    <span>Astronomy</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="travel" />
-                  <span>Travel</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobibes"
+                      value="gardening"
+                    />
+                    <span>Gardening</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="cinema" />
-                  <span>Cinema</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="photography"
+                    />
+                    <span>Photography</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="user-hobbies"
-                    value="videos-games"
-                  />
-                  <span>Video-Games</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input type="checkbox" name="user-hobbies" value="travel" />
+                    <span>Travel</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="drawing" />
-                  <span>Drawing</span>
-                </label>
-              </div>
+                <div>
+                  <label>
+                    <input type="checkbox" name="user-hobbies" value="cinema" />
+                    <span>Cinema</span>
+                  </label>
+                </div>
 
-              <div>
-                <label>
-                  <input type="checkbox" name="user-hobbies" value="animals" />
-                  <span>Animals</span>
-                </label>
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="videos-games"
+                    />
+                    <span>Video-Games</span>
+                  </label>
+                </div>
+
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="drawing"
+                    />
+                    <span>Drawing</span>
+                  </label>
+                </div>
+
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="user-hobbies"
+                      value="animals"
+                    />
+                    <span>Animals</span>
+                  </label>
+                </div>
               </div>
-            </div>
-            <input class="btn-login" type="submit" value="Register" />
-          </form>
+              <input class="btn-login" type="submit" value="Register" />
+            </form>
+          </Formik>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
