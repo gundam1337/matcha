05b72b83f33b6c18f9b6160d603ec9e3d42fa86f
gundam1337 <omarderkaoui1337@gmail.com>
2023-11-07 19:21:09 +0100

    working on the registration stack ...

---
+++ server/models/verificationTokensEmail.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const verificationTokenSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User' // Assuming 'User' is your user model
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600 // This token document will automatically be removed after 1 hour
+  }
+});
+
+const VerificationToken = mongoose.model('VerificationToken', verificationTokenSchema);
+
+module.exports = VerificationToken;
