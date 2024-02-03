226822a049f7444f650e7cc147fdcfbe72e38276
gundam1337 <omarderkaoui1337@gmail.com>
2024-02-03 20:15:58 +0100

    fix the url and image combo validation

--- server/controllers/profile/setProfile.js
+++ server/controllers/profile/setProfile.js
@@ -3,7 +3,8 @@ const Yup = require("yup");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // Max file size in bytes (e.g., 5MB)
-const urlPattern = /^https:\/\/storage\.googleapis\.com\/matcha-406014\.appspot\.com\/[a-f0-9\-]{36}.*\.(jpg|jpeg|png|gif)$/;
+const urlPattern =
+  /^https:\/\/storage\.googleapis\.com\/matcha-406014\.appspot\.com\/[a-f0-9\-]{36}.*\.(jpg|jpeg|png|gif)$/;
 
 const admin = require("firebase-admin");
 const serviceAccount = require("../../storage/matcha-406014-firebase-adminsdk-fnp82-8517b73387.json");
@@ -29,30 +30,29 @@ const nameValidationSchema = Yup.string()
   .max(40, "Name must be less than 40 characters")
   .required(" is required");
 
-
-  const fileSchema = Yup.mixed()
+const fileSchema = Yup.mixed()
   .test("fileSize", "File size must be less than 2 megabits", (value) => {
     return !value || (value && value.size <= MAX_FILE_SIZE);
   })
   .test("fileType", "Unsupported file format", (value) => {
     if (!value || !value.name) return true;
-    const extension = value.name.split('.').pop();
-    return extension === 'png' || extension === 'jpeg';
+    const extension = value.name.split(".").pop();
+    return extension === "png" || extension === "jpeg";
   });
-
 
 const urlSchema = Yup.string().matches(
   urlPattern,
-  'URL must follow the specified pattern'
+  "URL must follow the specified pattern"
 );
 
 const elementSchema = Yup.lazy((value) =>
-  typeof value === 'string' ? urlSchema : fileSchema
+  typeof value === "string" ? urlSchema : fileSchema
 );
 
-  //FIXME : the validation is not correct 
 const validationSchema = Yup.object({
-  image: Yup.array().of(elementSchema).length(2, 'Array must contain exactly 2 elements'),
+  image: Yup.array()
+    .of(elementSchema)
+    .length(2, "Array must contain exactly 2 elements"),
   info: Yup.object({
     firstName: nameValidationSchema,
     lastName: nameValidationSchema,
@@ -72,11 +72,10 @@ const validationSchema = Yup.object({
   }),
 });
 
-
 const storage = multer.memoryStorage();
 
-
 //Initialize multer with the custom storage engine
+//will be stored in the memory (RAM) of the server as a buffer
 const upload = multer({ storage: storage });
 
 const validate = async (req, res, next) => {
@@ -88,29 +87,33 @@ const validate = async (req, res, next) => {
     if (req.body.targetAge) req.body.targetAge = JSON.parse(req.body.targetAge);
 
     // Structure the data as expected by the validation schema
-    
-    const files = req.files || [];
-    // console.log("the request",req)
-    //console.log("files",files)
+    const uploadedFiles = req.files || [];
+    const requestBodyImages =
+      req.body && req.body.image
+        ? Array.isArray(req.body.image)
+          ? req.body.image
+          : [req.body.image]
+        : [];
+    const combinedMedia = [...uploadedFiles, ...requestBodyImages];
 
-    const imageUrls = Array.isArray(req.body.image) ? req.body.image : [req.body.image];
-    //console.log("image url",imageUrls)
+    const validMedia = combinedMedia.filter((mediaItem) => mediaItem);
 
-    const comboArray = [...files, ...imageUrls]
+    console.log("Filtered media items: ", validMedia);
 
-    const { image, ...restOfBody } = req.body;
+    // Ensure req.body is an object before destructuring
+    const { image, ...otherRequestBodyFields } =
+      req.body && typeof req.body === "object" ? req.body : {};
 
-    //console.log("the combination ",comboArray)
-    const dataToValidate = {
-      image: comboArray,
-      ...restOfBody,
+    const dataForValidation = {
+      image: validMedia, // Use validMedia instead of combinedMedia
+      ...otherRequestBodyFields,
     };
-    console.log("request files are",dataToValidate)
 
     // Validate the structured data
-    await validationSchema.validate(dataToValidate);
 
-    // Continue with your logic if validation is successful
+    await validationSchema.validate(dataForValidation);
+
+    req.body.image = dataForValidation.image;
     next();
   } catch (err) {
     //console.log("Validation error:", err);
@@ -120,8 +123,8 @@ const validate = async (req, res, next) => {
 
 //NOTE : UPLOAD the files into fire base
 //DONE : the user can't have more then two images
-//TODO : create for every user a bucket and this bucket only can caontain 2 images 
-//TODO : only upload the file with the file 
+//TODO : create for every user a bucket and this bucket only can caontain 2 images
+//TODO : only upload the file with the file
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -131,13 +134,13 @@ admin.initializeApp({
 const bucket = admin.storage().bucket();
 
 const uploadToFirebaseStorage = (req, res, next) => {
+  
   if (!req.files) {
     res.status(500).json({ error: "No files to upload." });
     return;
   }
 
   const files = req.files;
-  console.log("req files", req.files);
 
   let fileUploads = files.map((file) => {
     const blob = bucket.file(uuidv4() + file.originalname);
@@ -158,8 +161,6 @@ const uploadToFirebaseStorage = (req, res, next) => {
         const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
         resolve(publicUrl);
       });
-
-      console.log("the file buffer", file.buffer);
       blobStream.end(file.buffer);
     });
   });
@@ -174,7 +175,6 @@ const uploadToFirebaseStorage = (req, res, next) => {
       next(error);
     });
 };
-
 
 //TODO : add the the field the the profile is set up
 const setProfile = async (req, res, next) => {
@@ -210,7 +210,6 @@ const setProfile = async (req, res, next) => {
       ? 18
       : parseInt(req.body.targetAge.maxAge, 10);
 
-    //const birthDate = new Date(userInfo.info.birthdate);
     // Update the user's profile
     user.profile = {
       ...user.profile, // Keep existing profile fields
@@ -260,7 +259,7 @@ const getProfile = async (req, res) => {
     const user = await User.findOne({ username, email }).lean();
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Assuming that the user schema has a 'profile' field with nested properties
@@ -272,14 +271,16 @@ const getProfile = async (req, res) => {
       birthdate: profile.birthdate,
       phoneNumber: profile.phoneNumber,
       gender: profile.gender,
-      location: profile.location ? {
-        latitude: profile.location.latitude,
-        longitude: profile.location.longitude,
-        city: profile.location.city,
-        country: profile.location.country
-      } : undefined,
+      location: profile.location
+        ? {
+            latitude: profile.location.latitude,
+            longitude: profile.location.longitude,
+            city: profile.location.city,
+            country: profile.location.country,
+          }
+        : undefined,
       bio: profile.bio,
-      interests: profile.hobbies // Assuming hobbies is the correct field name
+      interests: profile.hobbies, // Assuming hobbies is the correct field name
     };
 
     res.json(profileData);
@@ -295,4 +296,4 @@ const profileSetup = [
   setProfile,
 ];
 
-module.exports = {profileSetup,getProfile};
+module.exports = { profileSetup, getProfile };
